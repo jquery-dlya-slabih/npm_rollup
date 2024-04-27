dcdd8d2d9d5190244bbcb0fdf21e30f2d18050da
Denis <denredsky@mail.ru>
2024-04-27 13:42:17 +0300

    - used node externals package instead of auto external

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,16 +2,15 @@ import terser from '@rollup/plugin-terser';
 import filesize from 'rollup-plugin-filesize';
 import postcss from 'rollup-plugin-postcss';
 import postcssUrl from 'postcss-url';
-import autoExternal from 'rollup-plugin-auto-external';
 import image from '@rollup/plugin-image';
 import typescript from '@rollup/plugin-typescript';
+import nodeExternals from 'rollup-plugin-node-externals';
 
 export default {
   input: 'src/index.ts',
   output: [
     {
       dir: 'dist',
-      format: 'es',
       entryFileNames: '[name].es.js',
       preserveModules: true
     },
@@ -19,11 +18,9 @@ export default {
       dir: 'dist',
       format: 'cjs',
       entryFileNames: '[name].js',
-      preserveModules: true,
-      exports: 'auto'
+      preserveModules: true
     }
   ],
-  external: ['react/jsx-runtime'],
   plugins: [
     typescript({
       include: ['src/**', '@types/**'],
@@ -36,8 +33,8 @@ export default {
       extract: 'index.css',
       plugins: [postcssUrl({ url: 'inline' })]
     }),
+    nodeExternals(),
     terser(),
-    autoExternal(),
     filesize({
       showMinifiedSize: false,
       showGzippedSize: false
